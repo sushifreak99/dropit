ad9b98c2b941c11fd9ecb134f7fc7734946ae8c6
sushifreak99 <boaz.ost.dev.24@gmail.com>
2023-07-30 13:14:54 +0300

    Add react router
    Signed-off-by: sushifreak99 <boaz.ost.dev.24@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,28 @@ import CartPage from './pages/cart';
 import CatalogPage from './pages/catalog';
 import { CartProvider } from './utils/cartContext';
 import { CatalogProvider } from './utils/catalogContext';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <CatalogPage />,
+    },
+    {
+      path: "/cart",
+      element: <CartPage />,
+    },
+  ]);
+
   return (
     <CatalogProvider>
       <CartProvider>
-        <CatalogPage />
-        <CartPage />
+        <RouterProvider router={router} />
       </CartProvider>
     </CatalogProvider>
   )
--- src/components/cartBadge.tsx
+++ src/components/cartBadge.tsx
@@ -1,23 +1,35 @@
 import { ShoppingCartOutlined } from "@ant-design/icons";
-import { Badge, BadgeProps, GlobalToken, theme } from "antd";
+import { Button, Badge, BadgeProps, GlobalToken, theme } from "antd";
 import { computeCartQuantity } from "../utils/cartContext";
+import { useNavigate } from "react-router-dom";
 
 interface CartBadgeProps {
-  count: number;
-  badgeSize: BadgeProps['size'];
-  cartFontSize: GlobalToken['fontSize'];
+  count: number,
+  badgeSize: BadgeProps['size'],
+  cartFontSize: GlobalToken['fontSize'],
+  onClick: () => void,
 }
 
-const CartBadge = ({count, badgeSize, cartFontSize}: CartBadgeProps) => (
-  <Badge count={count} size={badgeSize}>
-    <ShoppingCartOutlined style={{fontSize: cartFontSize}} />
-  </Badge>
+const CartBadge = ({count, badgeSize, cartFontSize, onClick}: CartBadgeProps) => (
+  <Button onClick={onClick}>
+    <Badge count={count} size={badgeSize}>
+      <ShoppingCartOutlined style={{fontSize: cartFontSize}} />
+    </Badge>
+  </Button>
 )
 
 const ConnectedCartBadge = () => {
   const { token } = theme.useToken();
   const count = computeCartQuantity();
-  return <CartBadge badgeSize="small" cartFontSize={token.fontSizeXL} count={count} />
+  const navigate = useNavigate();
+  return (
+    <CartBadge
+      badgeSize="small"
+      cartFontSize={token.fontSizeXL}
+      count={count}
+      onClick={() => navigate("/cart")}
+    />
+  )
 }
 
 export default ConnectedCartBadge;
